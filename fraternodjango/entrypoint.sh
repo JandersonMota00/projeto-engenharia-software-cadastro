3b1b5c9ad3d7c6b240e7ba7e32a18fd3bc8edf8a
Filipe Silva <47748436+KerusMighos@users.noreply.github.com>
2024-09-22 22:49:23 -0300

    docker config

--- fraternodjango/entrypoint.sh
+++ fraternodjango/entrypoint.sh
@@ -4,6 +4,9 @@ python manage.py collectstatic --noinput
 
 python manage.py spectacular --color --file schema.yml
 
-python manage.py makemigrations
-python manage.py migrate
-gunicorn -b :8000 appname.wsgi
+# python manage.py makemigrations
+# python manage.py migrate
+
+python manage.py devseed
+
+gunicorn -b :8000 fraternodjango.wsgi:application
